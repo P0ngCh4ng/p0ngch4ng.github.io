20dadd3f927b91a7f30c439ec14404ad91cca640
P0ngCh4ng <p0ngch4ng@gmail.com>
2022-08-28 00:44:25 +0900

    config: デプロイ設定6

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: process.env.GITHUB_ACTIONS && "/",
+  basePath: process.env.GITHUB_ACTIONS && "/p0ngch4ng.github.io",
   trailingSlash: true,
 };
 module.exports = nextConfig;
